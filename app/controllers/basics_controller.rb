cebf4710071ddf636e1596b7e84f7f53ed42f6e4
Freetown5 <iconteh21@yahoo.com>
2015-06-05 09:37:30 -0400

    pdf crowd stuff

--- app/controllers/basics_controller.rb
+++ app/controllers/basics_controller.rb
@@ -47,4 +47,22 @@ class BasicsController < ApplicationController
       return params[:basic].permit(:yname, :ycity, :yphone, :yemail, :cname, :ccity, :cphone, :cemail, :date, :POClastname, :POCfirstname, :POCtitle, :reference, :position_info, :skills)
     end
 
+    def generatePdf
+  begin
+    # create an API client instance
+    client = Pdfcrowd::Client.new("Freetown5", "9053fc3b9beb9dd5fb071bb71d6628f4")
+
+    # convert a web page and store the generated PDF to a variable
+    pdf = client.convertURI("http://www.google.com")
+
+    # send the generated PDF
+    send_data(pdf,
+              :filename => "google_com.pdf",
+              :type => "application/pdf",
+              :disposition => "attachment")
+  rescue Pdfcrowd::Error => why
+    render :text => why
+  end
+end
+
   end
